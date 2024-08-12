45a6bf091a0db2e92e3d0d88657f657984426ee0
Anthony Rivas <antho.snsd@gmail.com>
2024-08-12 12:20:16 -0500

    Modificado settings.py para configurar postgressql como base de datos y generado requirements.txt

--- sistemaDeNotas/settings.py
+++ sistemaDeNotas/settings.py
@@ -75,10 +75,17 @@ WSGI_APPLICATION = 'sistemaDeNotas.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+  'default': {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': getenv('PGDATABASE'),
+    'USER': getenv('PGUSER'),
+    'PASSWORD': getenv('PGPASSWORD'),
+    'HOST': getenv('PGHOST'),
+    'PORT': getenv('PGPORT', 5432),
+    'OPTIONS': {
+      'sslmode': 'require',
+    },
+  }
 }
 
 
