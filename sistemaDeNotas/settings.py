3ba131cddb1275d865663f22e56f551466637cfd
Anthony Rivas <antho.snsd@gmail.com>
2024-08-13 11:36:11 -0500

    Modificado settings.py para obtener Allowed_hosts desde variable de entorno y creado las apps auth, alumnos y calificaciones.

--- sistemaDeNotas/settings.py
+++ sistemaDeNotas/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = getenv("DJANGO_ALLOWED_HOSTS", "localhost,127.0.0.1,[::1]").split(",")
 
 
 # Application definition
