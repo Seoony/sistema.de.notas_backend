81c398271a150e4a087a9b87bfc4d628d73df6f8
Anthony Rivas <antho.snsd@gmail.com>
2024-08-20 21:35:35 -0500

    Removido la app Auth por UsersAuth debido a similitud con auth de django, añadido las migraciones y UsersAuth añadido en settings.py.

--- sistemaDeNotas/settings.py
+++ sistemaDeNotas/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    #'auth.apps.AuthConfig',
+    'usersAuth.apps.UsersauthConfig',
 ]
 
 MIDDLEWARE = [
@@ -83,7 +86,7 @@ DATABASES = {
     'HOST': getenv('PGHOST'),
     'PORT': getenv('PGPORT', 5432),
     'OPTIONS': {
-      'sslmode': 'require',
+      #'sslmode': 'require',
     },
   }
 }
@@ -129,3 +132,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'usersAuth.User'
---
+++ usersAuth/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+
+# Create your models here.
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, nombres, apellido_pat, apellido_mat,
+                    password=None):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(
+          email=email,
+          nombres=nombres,
+          apellido_pat=apellido_pat,
+          apellido_mat=apellido_mat
+        )
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, nombres, apellido_pat,
+                        apellido_mat, password=None):
+        user = self.create_user(email, nombres, apellido_pat,
+                                apellido_mat,password)
+        user.is_admin = True
+        user.is_staff = True
+        user.save()
+        return user
+      
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True, max_length=125)
+    dni = models.CharField(max_length=8, unique=True)
+    nombres = models.CharField(max_length=63)
+    apellido_pat = models.CharField(max_length=31)
+    apellido_mat = models.CharField(max_length=31)
+    fecha_nac = models.DateField(blank=True, null=True)
+    celular = models.CharField(max_length=9, blank=True, null=True)
+    est_reg = models.CharField(max_length=1, default='A')
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['nombre', 'apellido_pat', 'apellido_mat'
+                       , 'dni']
+    
+    objects = CustomUserManager()
+    
+    def __str__(self):
+        return self.nombres.split(" ")[0] + ' ' + self.apellido_pat
